4609e5191aa3ee57aed826b3b7ce2e28c8c7961f
Aly Nasser <ng.alynasser@gmail.com>
2020-09-29 15:03:40 +0200

    fix: members slider

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from '../shared/shared.module';
 import { buildIconLibrary } from './icon-library';
-import {
-  SwiperModule,
-  SwiperConfigInterface,
-  SWIPER_CONFIG,
-} from 'ngx-swiper-wrapper';
 
 import { NavComponent } from './components/nav/nav.component';
 import { HomeModule } from '../pages/home/home.module';
@@ -16,33 +11,6 @@ import { NavMobileComponent } from './components/nav-mobile/nav-mobile.component
 
 const CORE_COMPONENTS = [FooterComponent, NavComponent, NavMobileComponent];
 
-const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
-  slidesPerView: 4,
-  spaceBetween: 15,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
-  breakpoints: {
-    992: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 20,
-    },
-    1500: {
-      slidesPerView: 6,
-      spaceBetween: 20,
-    },
-    400: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-  },
-};
-
 @NgModule({
   declarations: [
     ...CORE_COMPONENTS,
@@ -50,9 +18,8 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     FooterComponent,
     NavMobileComponent,
   ],
-  imports: [SharedModule, BrowserModule, HomeModule, SwiperModule],
+  imports: [SharedModule, BrowserModule, HomeModule],
   exports: [...CORE_COMPONENTS],
-  providers: [{ provide: SWIPER_CONFIG, useValue: DEFAULT_SWIPER_CONFIG }],
 })
 export class CoreModule {
   constructor(library: FaIconLibrary) {
--- src/app/pages/home/components/members/members.component.ts
+++ src/app/pages/home/components/members/members.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, ElementRef } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-members',
@@ -7,5 +8,36 @@ import { Component, ChangeDetectionStrategy, ElementRef } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MembersComponent {
+  options: SwiperConfigInterface = {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    breakpoints: {
+      400: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+
+      600: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      1500: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
+  };
   constructor() {}
 }
